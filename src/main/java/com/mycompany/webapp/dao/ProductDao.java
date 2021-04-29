adca81688780a06f3dcb9ee5dc5e4472e442fed0
sora0420 <thfk0420@naver.com>
2021-04-29 15:31:31 +0900

    product

--- src/main/java/com/mycompany/webapp/dao/ProductDao.java
+++ src/main/java/com/mycompany/webapp/dao/ProductDao.java
@@ -6,14 +6,15 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.mycompany.webapp.dto.Pager;
 import com.mycompany.webapp.dto.Product;
-import com.mycompany.webapp.dto.Products;
-import com.mycompany.webapp.dto.Product;
 
 @Mapper
 public interface ProductDao {
 	public List<Product> pSelectByPage(Pager pager); //상품 전체 조회
+	public List<Product> pCategoryByPage(Pager pager); //상품 전체 조회
+	
 	public void pInsert(Product product); //상품 입력
-	public int count();
+	public int countAll();
+	public int count(int category);
 	public void pUpdate(Product product); //상품 업데이트
 	public Product pSelectByPno(int productno); //상품인덱스번호 찾기
 	public int pDeleteByPno(int productno); //상품 삭제
